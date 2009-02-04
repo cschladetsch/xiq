884e122400ae0fc188d472710d25894d85508806
christian.schladetsch <christian.schladetsch@d54a3f0e-f248-11dd-b556-9305e745af9e>
2009-02-04 08:32:23 +0000

    added test phase

---
+++ trunk/src/Phase.Boot.cpp
@@ -0,0 +1,21 @@
+// (C) 2009 christian.schladetsch@gmail.com
+
+#include "PhaseCommon.h"
+
+namespace Phase
+{
+	void Boot::Prepare()
+	{
+	}
+
+	bool Boot::Update(GameTime)
+	{
+		return true;
+	}
+
+	void Boot::Draw(Matrix const &)
+	{
+	}
+}
+
+//EOF
